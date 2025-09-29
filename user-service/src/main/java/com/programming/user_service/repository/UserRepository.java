963bfea1df16f3556f12762a4a119e468fe0e812
minhkhoi204 <98089559+minhkhoi204@users.noreply.github.com>
2025-09-29 19:42:17 +0700

    Add files via upload

---
+++ user-service/src/main/java/com/programming/user_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.programming.user_service.repository;
+
+import com.programming.user_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+
+    @Override
+    boolean existsById(Long id);
+}
