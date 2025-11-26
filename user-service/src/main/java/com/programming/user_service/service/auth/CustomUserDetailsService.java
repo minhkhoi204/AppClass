21c0bb199db1a77100d4a4ffb0d5b054dab8a296
minhkhoi204 <nvmkhoi20@gmail.com>
2025-11-26 19:43:57 +0700

    add user detail service

---
+++ user-service/src/main/java/com/programming/user_service/service/auth/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.programming.user_service.service.auth;
+
+import com.programming.user_service.config.CustomUserDetails;
+import com.programming.user_service.domain.model.User;
+import com.programming.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+        
+        return new CustomUserDetails(user);
+    }
+}
