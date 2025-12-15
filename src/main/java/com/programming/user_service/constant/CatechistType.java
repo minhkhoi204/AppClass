d1dce32fdc46894f0be81b37107910a697300886
minhkhoi204 <nvmkhoi20@gmail.com>
2025-12-15 23:34:54 +0700

    remove abundant folders and files

--- src/main/java/com/programming/user_service/constant/CatechistType.java
+++
@@ -1,6 +0,0 @@
-package com.programming.user_service.constant;
-
-public enum CatechistType {
-    DU_TRUONG,
-    HUYNH_TRUONG
-}
--- src/main/java/com/programming/user_service/request/UpdateUserRequest.java
+++
@@ -1,7 +0,0 @@
-package com.programming.user_service.request;
-
-public class UpdateUserRequest {
-    private String fullName;
-    private String email;
-    private String phone;
-}
