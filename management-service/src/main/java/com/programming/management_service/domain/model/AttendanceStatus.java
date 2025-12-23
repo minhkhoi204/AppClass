3ea55257e36704c60aa598f58f943b8d98ed0827
minhkhoi204 <nvmkhoi20@gmail.com>
2025-12-23 11:47:15 +0700

    create attendance status

---
+++ management-service/src/main/java/com/programming/management_service/domain/model/AttendanceStatus.java
@@ -0,0 +1,18 @@
+package com.programming.management_service.domain.model;
+
+public enum AttendanceStatus {
+    PRESENT("Có mặt"),
+    ABSENT("Vắng mặt"),
+    LATE("Đi muộn"),
+    EXCUSED("Vắng có phép");
+
+    private final String displayName;
+
+    AttendanceStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
