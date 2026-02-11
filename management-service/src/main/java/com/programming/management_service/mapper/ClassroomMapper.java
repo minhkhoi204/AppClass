1ffe37193d69ca0bce88c651a5ac5fbaf193ebd2
minhkhoi204 <nvmkhoi20@gmail.com>
2026-02-11 23:27:02 +0700

    adjust classroom mappper (add class order, next classroom, is final class)

--- management-service/src/main/java/com/programming/management_service/mapper/ClassroomMapper.java
+++ management-service/src/main/java/com/programming/management_service/mapper/ClassroomMapper.java
@@ -22,6 +22,9 @@ public class ClassroomMapper {
         //c.setMaxStudents(dto.getMaxStudents());
         c.setSchedule(dto.getSchedule());
         c.setNote(dto.getNote());
+        c.setClassOrder(dto.getClassOrder());
+        c.setNextClassroomName(dto.getNextClassroomName());
+        c.setIsFinalClass(dto.getIsFinalClass() != null ? dto.getIsFinalClass() : false);
         // c.setStudentIds(dto.getStudentIds() != null ? new HashSet<>(dto.getStudentIds()) : new HashSet<>());
         // c.setCatechistIds(dto.getCatechistIds() != null ? new HashSet<>(dto.getCatechistIds()) : new HashSet<>());
         return c;
@@ -37,6 +40,9 @@ public class ClassroomMapper {
         //dto.setMaxStudents(classroom.getMaxStudents());
         dto.setSchedule(classroom.getSchedule());
         dto.setNote(classroom.getNote());
+        dto.setClassOrder(classroom.getClassOrder());
+        dto.setNextClassroomName(classroom.getNextClassroomName());
+        dto.setIsFinalClass(classroom.getIsFinalClass());
         // dto.setStudentIds(classroom.getStudentIds());
         // dto.setCatechistIds(classroom.getCatechistIds());
         return dto;
