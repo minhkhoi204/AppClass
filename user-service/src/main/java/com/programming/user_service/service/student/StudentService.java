d28f09f6a00c5f52ed21e6f3bb0dde97b4604f21
minhkhoi204 <nvmkhoi20@gmail.com>
2026-02-11 12:13:31 +0700

    adjust student service (add func get students by ids)

--- user-service/src/main/java/com/programming/user_service/service/student/StudentService.java
+++ user-service/src/main/java/com/programming/user_service/service/student/StudentService.java
@@ -3,10 +3,15 @@ package com.programming.user_service.service.student;
 import com.programming.common.common_dto.student.StudentRequestDto;
 import com.programming.common.common_dto.student.StudentResponseDto;
 
+import java.util.List;
+import java.util.Set;
+
 public interface StudentService {
     StudentResponseDto createStudent(StudentRequestDto studentDto);
 
     StudentResponseDto getStudentById(Long studentId);
+    
+    List<StudentResponseDto> getStudentsByIds(Set<Long> studentIds);
 
     StudentResponseDto createStudentWithUserId(StudentRequestDto studentDto, Long userId);
 
