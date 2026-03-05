982f705c2d9fd381ab35452c001f119472d5673d
minhkhoi204 <nvmkhoi20@gmail.com>
2026-03-05 11:50:20 +0700

    import util.list

--- user-service/src/main/java/com/programming/user_service/controller/StudentController.java
+++ user-service/src/main/java/com/programming/user_service/controller/StudentController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/students")
